b17c9add0cfc60152bac0650a60a1740ef784a92
roryhackney <roryhackney@gmail.com>
2021-04-30 20:42:21 -0700

    Express and Handlebars

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
+'use strict'
 import {getAll, getItem} from "./data.js";
-import qs from 'qs';
 
-import http from "http"; //import HTTP
-import fs, { readFile } from "fs"; //import file system
-import { query } from "express";
+import exphbs from 'express-handlebars'; //handlebars
+import express from 'express'; //express framework
 
-http.createServer((req, res) => { //create a server with request and response
-    let url = req.url.split("?");
-    let query = qs.parse(url[1]);
-    let path = url[0].toLowerCase(); //get request URL
-    switch(path) { //each path / route / url has its own content
-        case '/':
-            res.writeHead(200, {'Content-Type': 'text/plain'}); //response header contains content type & status code 200 which means success
-            res.end(JSON.stringify(getAll()));
-            break;
-        case '/about':
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end("Rory Hackney is a web developer, programmer, and artist from Seattle, Washington. They enjoy kissing cats, drawing comics, and being too anxious to function.");
-            break;
-        case '/detail':
-            res.writeHead(200, {'Content-Type':'text/plain'});
-            let dino = getItem(query.name);
-            if(dino === undefined){res.end("Dino Not Found")}
-            else {res.end(JSON.stringify(dino));}
-            break;
-        default:
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.end('Not Found');
-            break;
+const app = express();
+app.set('port', process.env.PORT || 3000);
+app.use(express.static('public')); // set location for static files
+app.use(express.urlencoded({extended: true})); //Parse URL-encoded bodies
+
+app.engine("handlebars", exphbs({defaultLayout: false}));
+app.set("view engine", "handlebars");
+
+
+//routes
+app.get('/', (req, res) => {
+    res.type('text/html');
+    let dinos= getAll();
+    res.render('home', {dinos: dinos, name: dinos.name})
+});
+
+app.get('/about', (req, res) => {
+    res.type('text/plain');
+    res.send("Rory Hackney is a web developer, programmer, and artist from Seattle, Washington. They enjoy kissing cats, drawing comics, and being too anxious to function.");
+});
+
+app.get('/detail', (req, res) => {
+    res.type('text/html');
+    let query = req.query;
+    if(query.name === undefined){res.send('Dino Not Found')}
+    else {
+        let dino = getItem(query.name);
+        // console.log(dino);
+        if (dino===undefined){res.send("Dino Not Found");}
+        else {res.render('detail', {dino: dino});};
     }
-}).listen(process.env.PORT || 3000);
+});
+
+app.use((req, res) => {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 Error - File Not Found');
+});
+
+app.listen(app.get('port'), () => {
+    console.log('Express server started');
+});
